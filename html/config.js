32d4ae5cd871f32ed3b0469db1ae00d41b884195
Adam <87525637+sadam376@users.noreply.github.com>
2025-08-30 12:21:15 +0200

    Update config.js

--- html/config.js
+++ html/config.js
@@ -9,48 +9,49 @@
 var staff_team = [
 
 	{
-		"name":"Nevera Owner",
-		"image":"https://forum.cfx.re/user_avatar/forum.cfx.re/neveradevelopment/144/4447132_2.png",
+		"name":"Szerver Tulaj",
+		"image":"https://forum.cfx.re/user_avatar/forum.cfx.re/infinity_adam/144/4447132_2.png",
 		"rank":"Owner"
 	},
 	{
-		"name":"Nevera Admin",
-		"image":"https://forum.cfx.re/user_avatar/forum.cfx.re/neveradevelopment/144/4447132_2.png",
+		"name":"Szerver admin",
+		"image":"https://forum.cfx.re/user_avatar/forum.cfx.re/infinity_adam/144/4447132_2.png",
 		"rank":"Admin"
 	}
 
 ]
 
 const tipsConfig = [
-    {
-        title: "Tip 1 (short)",
-        text: "Always wear your seatbelt when driving in Los Santos.",
-        img: "https://placehold.co/314x176/EEE/31343C",
-        timeout: 10 // seconds
-    },
-    {
-        title: "Tip 2 (long)",
-        text: "Explore different jobs on the server, like taxi driving, delivery missions, or even criminal activities. Each job has its own rewards and risks. Remember to manage your time wisely, avoid unnecessary conflicts, and cooperate with other players to maximize your gains and enjoy the roleplay experience to the fullest. Take advantage of special events and seasonal activities to earn unique rewards. Always keep an eye on the map for dynamic missions and spontaneous opportunities, and make sure your character is properly equipped for any challenge that comes your way. Building relationships with other players can also unlock new roles and adventures, making every session unpredictable and exciting.",
-        img: "https://placehold.co/314x176/EEE/31343C",
-        timeout: 10 // seconds
-    },
-    {
-        title: "Tip 3 (empty-image)",
-        text: "Keep backup gear: weapons, vehicles, and medical kits are essential for surviving unexpected situations.",
-        img: "",
-        timeout: 10 // seconds
-    },
-    {
-        title: "Tip 4 (no image)",
-        text: "Read the server rules carefully and respect roleplay guidelines. Following the rules ensures a better experience for everyone.",
-        timeout: 10 // seconds
-    },
-    {
-        title: "Tip 5 (local-image)",
-        text: "Keep backup gear: weapons, vehicles, and medical kits are essential for surviving unexpected situations.",
-        img: "/tips/tip1.jpg",
-        timeout: 10 // seconds
-    },
+{
+    title: "Tipp 1 (rövid)",
+    text: "Mindig használd a biztonsági övet, amikor Los Santosban vezetsz.",
+    img: "https://placehold.co/314x176/EEE/31343C",
+    timeout: 10 // másodperc
+},
+{
+    title: "Tipp 2 (hosszú)",
+    text: "Fedezz fel különböző munkákat a szerveren, mint például taxisofőrködés, futárküldetések vagy akár bűnözői tevékenységek. Minden munkának megvannak a maga jutalmai és kockázatai. Ne feledd, hogy oszd be bölcsen az idődet, kerüld a felesleges konfliktusokat, és működj együtt más játékosokkal, hogy maximalizáld a nyereségedet és a lehető legjobban élvezd a szerepjáték élményt. Használd ki a különleges eseményeket és szezonális aktivitásokat, hogy egyedi jutalmakat szerezz. Mindig figyeld a térképet a dinamikus küldetések és spontán lehetőségek miatt, és gondoskodj arról, hogy a karaktered megfelelően fel legyen készülve minden kihívásra. Kapcsolatok építése más játékosokkal új szerepeket és kalandokat is megnyithat, így minden játékalkalom kiszámíthatatlan és izgalmas lehet.",
+    img: "https://placehold.co/314x176/EEE/31343C",
+    timeout: 10 // másodperc
+},
+{
+    title: "Tipp 3 (üres kép)",
+    text: "Legyen tartalék felszerelésed: fegyverek, járművek és elsősegélycsomagok nélkülözhetetlenek a váratlan helyzetek túléléséhez.",
+    img: "",
+    timeout: 10 // másodperc
+},
+{
+    title: "Tipp 4 (nincs kép)",
+    text: "Olvasd el figyelmesen a szerver szabályait, és tartsd be a szerepjáték irányelveit. A szabályok követése mindenki számára jobb élményt biztosít.",
+    timeout: 10 // másodperc
+},
+{
+    title: "Tipp 5 (helyi kép)",
+    text: "Legyen tartalék felszerelésed: fegyverek, járművek és elsősegélycsomagok nélkülözhetetlenek a váratlan helyzetek túléléséhez.",
+    img: "/tips/tip1.jpg",
+    timeout: 10 // másodperc
+},
+
 ];
 
 
@@ -74,13 +75,13 @@ const enableWinterUpdate = false
 
 
 // Text settings
-const name = "<strong>NEV</strong>ERA"
+const name = "<strong>FIVEM</strong>LAND"
 const underName = "ROLE<b>PLAY</b>"
-const desc = "FiveM Roleplay server featuring an extensive collection of custom scripts, maps, vehicles, unique weapons, and much more."
+const desc = "FiveM szerepjáték szerver";
 
 
 // Social media
-const discord = "https://discord.gg/NeveraDev/tw28AqrgWU"  // If = "" then icon will not show up on screen
+const discord = "https://discord.gg/uCt54UV3Sh"  // If = "" then icon will not show up on screen
 const instagram = ""	// https://example.com
 const youtube = "" 		// https://example.com
 const twitter = "" 		// https://example.com
@@ -117,4 +118,5 @@ const localAudio = false
 //-- LOCAL VIDEO
 // if enableLocalVideo is true, then loading will load "video.webm" file and play it except youtube video.
 // If localVideo is enabled, showYoutubeVideo is automatically disabled.
+
 // You can only import a video from either YouTube or local. Local video taking priority.
